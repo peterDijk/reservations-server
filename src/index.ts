dba11403c973b00aee38e56c3bc22862809b10c1
Peter van Dijk <petervandijk@gmail.com>
2020-05-21 00:12:04 +0200

    socket.io is on

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { useKoaServer } from 'routing-controllers';
 import * as Koa from 'koa';
 import { Server } from 'http';
-// import { secret } from './jwt';
+import { secret } from './jwt';
 import * as IO from 'socket.io';
-// import * as socketIoJwtAuth from 'socketio-jwt-auth';
+import * as socketIoJwtAuth from 'socketio-jwt-auth';
 import { authorizationChecker } from './lib/helpers/authorizationChecker';
 
 import Service from './service/controller';
@@ -20,22 +20,22 @@ useKoaServer(app, {
   authorizationChecker,
 });
 
-// io.use(
-//   socketIoJwtAuth.authenticate({ secret }, async (payload, done) => {
-//     // const user = await User.findOne(payload.id);
-//     // if (user) done(null, user); else {
-//     done(null, false, `Invalid JWT user ID`);
-//   }),
-// );
+io.use(
+  socketIoJwtAuth.authenticate({ secret }, async (payload, done) => {
+    // const user = await User.findOne(payload.id);
+    // if (user) done(null, user); else {
+    done(null, false, `Invalid JWT user ID`);
+  }),
+);
 
-// io.on('connect', (socket) => {
-//   const name = socket.request.user.firstName;
-//   console.log(`User ${name} just connected`);
+io.on('connect', (socket) => {
+  const name = socket.request.user.firstName;
+  console.log(`User ${name} just connected`);
 
-//   socket.on('disconnect', () => {
-//     console.log(`User ${name} just disconnected`);
-//   });
-// });
+  socket.on('disconnect', () => {
+    console.log(`User ${name} just disconnected`);
+  });
+});
 
 // TODO: install logger tool
 try {
