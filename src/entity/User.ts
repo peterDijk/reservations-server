6b864ad2dfe20263e39273b60f3eb3f909daf958
Peter van Dijk <petervandijk@gmail.com>
2020-05-21 14:26:05 +0200

    with User entity

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,55 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { IsString, MinLength, IsEmail } from 'class-validator';
+import { Exclude } from 'class-transformer';
+import * as bcrypt from 'bcrypt';
 
 @Entity()
-export class User {
+class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @IsString()
+  @MinLength(2)
+  @Column('text')
+  memberNumber: string;
 
-    @Column()
-    firstName: string;
+  @IsString()
+  @MinLength(2)
+  @Column('text')
+  firstName: string;
 
-    @Column()
-    lastName: string;
+  @IsString()
+  @MinLength(2)
+  @Column('text')
+  lastName: string;
 
-    @Column()
-    age: number;
+  @IsEmail()
+  @Column('text')
+  email: string;
 
+  @IsString()
+  @MinLength(5)
+  @Column('text')
+  @Exclude({ toPlainOnly: true }) // what does this do ? transformation?
+  password: string;
+
+  @Column('boolean', { nullable: false, default: false })
+  isAdmin: boolean;
+
+  @Column('boolean', { nullable: false, default: false })
+  isSuperAdmin: boolean;
+
+  //   @OneToMany(_ => Ticket, ticket => ticket.user)
+  //   tickets: Ticket[]
+
+  async setPassword(rawPassword: string) {
+    const hash = await bcrypt.hash(rawPassword, 10);
+    this.password = hash;
+  }
+
+  checkPassword(rawPassword: string): Promise<boolean> {
+    return bcrypt.compare(rawPassword, this.password);
+  }
 }
+
+export default User;
