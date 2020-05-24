34b2412ed442df0c17a85820744142d87ce6356b
Peter van Dijk <petervandijk@gmail.com>
2020-05-24 21:33:44 +0200

    removed services creation (double) from migration 2

--- src/migration/1590332849475-AddAccountAndLocationModels.ts
+++ src/migration/1590332849475-AddAccountAndLocationModels.ts
@@ -1,20 +1,26 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddAccountAndLocationModels1590332849475 implements MigrationInterface {
-    name = 'AddAccountAndLocationModels1590332849475'
+export class AddAccountAndLocationModels1590332849475
+  implements MigrationInterface {
+  name = 'AddAccountAndLocationModels1590332849475';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "locations" ("id" SERIAL NOT NULL, "locationname" text NOT NULL, "capacity" text NOT NULL, "account_id" integer, CONSTRAINT "PK_7cc1c9e3853b94816c094825e74" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "accounts" ("id" SERIAL NOT NULL, "accountname" text NOT NULL, "account_description" text, "administrator" text NOT NULL, CONSTRAINT "PK_5a7a02c20412299d198e097a8fe" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "services" ("id" SERIAL NOT NULL, "message" text NOT NULL, "date_created" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "PK_ba2d347a3168a296416c6c5ccb2" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "locations" ADD CONSTRAINT "FK_18a15dc1faf8902c28bd42f2eca" FOREIGN KEY ("account_id") REFERENCES "accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "locations" ("id" SERIAL NOT NULL, "locationname" text NOT NULL, "capacity" text NOT NULL, "account_id" integer, CONSTRAINT "PK_7cc1c9e3853b94816c094825e74" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "accounts" ("id" SERIAL NOT NULL, "accountname" text NOT NULL, "account_description" text, "administrator" text NOT NULL, CONSTRAINT "PK_5a7a02c20412299d198e097a8fe" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "locations" ADD CONSTRAINT "FK_18a15dc1faf8902c28bd42f2eca" FOREIGN KEY ("account_id") REFERENCES "accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "locations" DROP CONSTRAINT "FK_18a15dc1faf8902c28bd42f2eca"`);
-        await queryRunner.query(`DROP TABLE "services"`);
-        await queryRunner.query(`DROP TABLE "accounts"`);
-        await queryRunner.query(`DROP TABLE "locations"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "locations" DROP CONSTRAINT "FK_18a15dc1faf8902c28bd42f2eca"`,
+    );
+    await queryRunner.query(`DROP TABLE "accounts"`);
+    await queryRunner.query(`DROP TABLE "locations"`);
+  }
 }
