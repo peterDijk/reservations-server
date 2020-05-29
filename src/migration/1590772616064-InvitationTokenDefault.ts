1862f9b9e048b4f86ada310ec935aa6bfee55812
Peter van Dijk <petervandijk@gmail.com>
2020-05-29 19:28:52 +0200

    changed name migration

--- src/migration/1590772616064-InvitationTokenDefault.ts
+++ src/migration/1590772616064-InvitationTokenDefault.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class InvitationTokenDefault1590772616064 implements MigrationInterface {
-  name = 'Needed1590772616064';
+  name = 'InvitationTokenDefault1590772616064';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
